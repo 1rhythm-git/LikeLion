8e3c4c8a69a8844e0f90ba38bd3836c1f9a837ac
1rhythm-git <dmdckd@gmail.com>
2025-02-21 16:15:22 +0900

    loading

---
+++ TodayTask1/TodayTask1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TodayTask1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //로딩바 시작화면
+            //게임스토리1
+
+            Console.WriteLine("엔터를 누르면 게임이 시작됩니다.");
+            Console.ReadLine();
+            Console.Clear();
+            Console.WriteLine(□□□□□□□□□□);
+            Console.ReadLine();
+
+            Console.WriteLine("ㅇㅇㅇㅇ");
+
+
+        }
+    }
+}
