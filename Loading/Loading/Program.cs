3e75a49ff6add572f770cff16b1d1b0e59849b13
1rhythm-git <dmdckd@gmail.com>
2025-02-21 15:04:20 +0900

    Study6

---
+++ Loading/Loading/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using System.Runtime.InteropServices;
+
+namespace Loading
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+
+
+
+            Console.WriteLine("□□□□□□□□□□");
+            Thread.Sleep(1000);
+            Console.Clear();
+            Console.WriteLine("■□□□□□□□□□");
+            Thread.Sleep(1000);
+            Console.Clear();
+            Console.WriteLine("■■□□□□□□□□");
+            Thread.Sleep(1000);
+            Console.Clear();
+            Console.WriteLine("■■■□□□□□□□");
+            Thread.Sleep(1000);
+            Console.Clear();
+            Console.WriteLine("                    ");
+            Thread.Sleep(1000);
+            Console.Clear();
+            Console.WriteLine("■■■■■□□□□□");
+            Thread.Sleep(1000);
+            Console.Clear();
+            Console.WriteLine("■■■■■■□□□□");
+            Thread.Sleep(1000);
+            Console.Clear();
+            Console.WriteLine("■■■■■■■□□□");
+            Thread.Sleep(1000);
+            Console.Clear();
+            Console.WriteLine("■■■■■■■■□□");
+            Thread.Sleep(1000);
+            Console.Clear();
+            Console.WriteLine("■■■■■■■■■□");
+            Thread.Sleep(1000);
+            Console.Clear();
+            Console.WriteLine("■■■■■■■■■■");
+            Console.Clear();
+            Console.WriteLine("택스트엘든링");
+
+            Console.WriteLine("스토리1");
+
+
+        }
+    }
+}
