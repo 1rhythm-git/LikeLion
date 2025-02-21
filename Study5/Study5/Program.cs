9cab55341d568cffc8c5a8360b87e3e6591fae1b
1rhythm-git <dmdckd@gmail.com>
2025-02-21 16:02:24 +0900

    Study7

---
+++ Study5/Study5/Program.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Study5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* //사용자 입력을 문자열 받기
+             Console.Write("이름을 입력하세요: ");
+             string userName = Console.ReadLine();
+
+             Console.WriteLine($"안녕하세요, {userName}님!");*/
+
+
+            /* //문자열을 정수로 변환
+             Console.WriteLine("나이를 입력하세요: ");
+             string input = Console.ReadLine();
+             int age = int.Parse(input);
+
+             Console.WriteLine($"내년에는 {age + 1} 살이 되겠군요!");
+             Console.WriteLine("내년에는 "+ age +"살이 되겠군요!");
+             Console.WriteLine("sosusdpsms {0} 살이 되겠군요!",age);*/
+
+
+            /*float Skill_Damage = 0.0f;
+            float Card = 0.0f;
+            float AwakeDamage = 0.0f;
+            int Max_Mana;
+            int Battle_Recovery;
+            int NonBattle_Recovery;
+            float Speed = 0.0f;
+            float Ride = 0.0f;
+            float Object_Speed = 0.0f;
+            float CoolDown = 0.0f;
+
+                                   
+            Console.Write("루인 스킬 피해량을 입력하세요: ");
+            Skill_Damage = float.Parse(Console.ReadLine());
+            Console.Write("카드 게이지 획득량을 입력하세요: ");
+            Card = float.Parse(Console.ReadLine());
+            Console.Write("각성기 피해량을 입력하세요: ");
+            AwakeDamage = float.Parse(Console.ReadLine());
+            Console.Write("최대 마나량을 입력하세요: ");
+            Max_Mana = int.Parse(Console.ReadLine());
+            Console.Write("전투 중 마나 회복량을 입력하세요: ");
+            Battle_Recovery = int.Parse(Console.ReadLine());
+            Console.Write("전투 중 마나 회복량을 입력하세요: ");
+            NonBattle_Recovery = int.Parse(Console.ReadLine());
+            Console.Write("이동속도를 입력하세요: ");
+            Speed = float.Parse(Console.ReadLine());
+            Console.Write("탈것 속도를 입력하세요: ");
+            Ride = float.Parse(Console.ReadLine());
+            Console.Write("운반속도를 입력하세요: ");
+            Object_Speed = float.Parse(Console.ReadLine());
+            Console.Write("스킬 재사용 대기시간 감소를 입력하세요: ");
+            CoolDown = float.Parse(Console.ReadLine());
+
+            
+            
+            Console.WriteLine($"루인 스킬 피해    : {Skill_Damage}%");
+            Console.WriteLine($"카드 게이지 획득량    : {Card}%");
+            Console.WriteLine($"각성기 피해량    : {AwakeDamage}%");
+            Console.WriteLine($"최대 마나량    : {Max_Mana}");
+            Console.WriteLine($"전투 중 마나 회복량    : {Battle_Recovery}");
+            Console.WriteLine($"비전투 중 마나 회복량    : {NonBattle_Recovery}");
+            Console.WriteLine($"이동속도    : {Speed}%");
+            Console.WriteLine($"탈것 속도    : {Ride}%");
+            Console.WriteLine($"운반속도    : {Ride}%");
+            Console.WriteLine($"스킬 재사용 대기시간 감소    : {Ride}%");*/
+
+
+
+
+
+        }
+
+        private static void Clear()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
