e440f3c549477c3850dbf6d5a28944885514a284
1rhythm-git <dmdckd@gmail.com>
2025-02-21 14:34:49 +0900

    Study5

--- Study4/Study4/Program.cs
+++ Study4/Study4/Program.cs
@@ -58,17 +58,132 @@ namespace Study4
             Console.WriteLine("인내 = "+g);
             Console.WriteLine("숙련 = "+h);*/
 
-            //숫자 데이터 형식: 정수와 실수를 다룰 때 사용하는 다양한 타입
+            /*//숫자 데이터 형식: 정수와 실수를 다룰 때 사용하는 다양한 타입
             int integerNum = 10;    //정수 데이터
             float floatNum = 3.14f; //단정밀도 실수
             double doubleNum = 3.14159; //배정밀도 실수
 
-            Console.WriteLine(integerNum);
-            Console.WriteLine(floatNum);
-            Console.WriteLine(doubleNum);
+            Console.WriteLine(integerNum);  //출력 10
+            Console.WriteLine(floatNum);    // 출력 3.14
+            Console.WriteLine(doubleNum);   // 출력 3.14159*/
 
 
+            /*//정수 데이터 형식: 소수점이 없는 숫자를 표현
+            int intValue = -100;    //4바이트 크기의 정수
+            long longValue = 1234567890L;   //8바이트 크기의 정수
 
+            Console.WriteLine(intValue);    //출력: -100
+            Console.WriteLine(longValue);   //출력: 1234567890*/
+
+
+            //1비트   0과1로 구분
+            //8비트   1바이트
+
+
+            /*//부호 있는 정수: 음수와 양수를 모두 표현 가능
+            sbyte signedByte = -50;   //1바이트 크기
+            short signedShort = -32000; //2바이트
+            int signedInt = -200000000; //4바이트
+
+            Console.WriteLine(signedByte);
+            Console.WriteLine(signedShort);
+            Console.WriteLine(signedInt);*/
+
+
+            /*//부호없는 정수 데이터 형식
+            byte unsignedByte = 255;    //1바이트 크기
+            ushort unsignedShort = 65000;  //2바이트 크기
+            uint unsignedInt = 4000000000;  //4바이트 크기
+
+            Console.WriteLine(unsignedByte);
+            Console.WriteLine(unsignedShort);
+            Console.WriteLine(unsignedInt);*/
+
+
+            /*//실수 데이터 형식
+            float singlePrecision = 3.14f;
+            double doublePrecision = 3.1415926535;
+            decimal decimalPrecision = 3.1415926535897932384626433833m;
+
+            Console.WriteLine(singlePrecision);
+            Console.WriteLine(doublePrecision);
+            Console.WriteLine(decimalPrecision);*/
+
+
+            /*//숫자 형식의 리터럴 값에 접미사 붙이기
+            int integerValue = 100;
+            long longValue = 100l;
+            float floatValue = 3.14f;
+            double doubleValue = 3.14;
+            decimal decimalValue = 3.14m;
+
+            Console.WriteLine(integerValue);
+            Console.WriteLine(longValue);
+            Console.WriteLine(floatValue);
+            Console.WriteLine(doubleValue);
+            Console.WriteLine(decimalValue);*/
+
+
+            /*//문자 데이터 형식
+            char letter = 'A';
+            char symbol = '#';
+            char number = '9';
+
+            Console.WriteLine("letter = "+letter);
+            Console.WriteLine("symbol = "+symbol);
+            Console.WriteLine("number = "+number);*/
+
+
+            //string 형식: 여러 문자를 저장
+            string greeting = "Hello World";
+            string name = "Alice";
+
+            /*Console.WriteLine(greeting);
+            Console.WriteLine(name);*/
+
+            /*Console.Write(greeting);
+            Console.Write(name);*/
+            /*
+                        Console.Write(greeting +" "+ name);*/
+
+
+            /*//bool형식    :   참(true) = 1     거짓(false) = 0
+            bool isRunning = true;
+            bool isFinished = false;
+
+            Console.WriteLine(isRunning);
+            Console.WriteLine(isFinished);*/
+
+
+            /*//const: 변하지 않는 값을 정의
+            const double Pi = 3.14159;
+            const int MaxScore = 100;
+
+            Console.WriteLine(Pi);
+            Console.WriteLine(MaxScore);*/
+
+
+            /*//닷넷 형식 : 기본 형식의 닷넷 표현
+            System.Int32 number = 123;
+            System.String text = "Hello";
+            System.Boolean flag = true;
+
+            Console.WriteLine(number);
+            Console.WriteLine(text);
+            Console.WriteLine(flag);*/
+
+
+            //int 래퍼 형식의 메서드 활용
+            int number = 123;
+            string numberAsString = number.ToString();
+
+
+            //bool 래퍼 형식의 메서드 활용
+            bool flag = true;
+            string flagAsString = flag.ToString();
+
+            Console.WriteLine(numberAsString);
+            Console.WriteLine(flagAsString);
 
 
         }
