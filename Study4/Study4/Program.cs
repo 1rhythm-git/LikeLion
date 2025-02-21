caf81aa7a075c6a4d0ab00adf7d9ddbe3bc5f435
1rhythm-git <dmdckd@gmail.com>
2025-02-21 11:51:53 +0900

    Study4
    ~교안6번까지

---
+++ Study4/Study4/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Study4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*//문자열
+            string greeting;   //문자열 변수를 선언
+            greeting = "Hello, World!";  //변수에 값을 저장
+
+            //변수의 값을 사용
+            Console.WriteLine(greeting);    //출력 : Hello, World!*/
+
+
+            /*//변수 선언과 초기화를 한 번에 수행
+            int score = 100;    //정수형 변수 선언과 동시에 100으로 초기화
+            double temperature = 36.5;  //실수형 변수 선언과 초기화
+            string city = "Seoul";  //문자열 변수 선언과 초기화
+
+            //변수 출력
+            Console.WriteLine("점수 = "+score);
+            Console.WriteLine("온도 = "+temperature);
+            Console.WriteLine("도시 = "+city);*/
+
+
+            /* //같은 데이터 타입의 변수를 쉼표로 구분해 한 번에 선언
+             int x = 10, y = 20, z = 30; //정수형 변수 x,y,z를 선언하고 초기화
+
+             Console.WriteLine("x = "+x);
+             Console.WriteLine("y = "+y);
+             Console.WriteLine("z = "+z);*/
+
+            /*//상수: 값을 변경할 수 없는 변수
+            const double Pi = 3.14159;  //상수 Pi 선언 및 초기화
+            const int MaxScore = 100;   //정수형 상수 선언
+
+            //출력
+            Console.WriteLine("Pi = "+Pi);  //출력 Pi
+            Console.WriteLine("MaxScore = "+MaxScore);    //출력 MaxScore*/
+
+            //
+
+            int a = 16755, b = 78103, c = 36, d = 1017, e = 41, f = 611, g = 22, h = 39;
+
+            Console.WriteLine("공격력 = "+a);
+            Console.WriteLine("최대 생명력 = "+b);
+            Console.WriteLine(" ");
+            Console.WriteLine("치명 = "+c);
+            Console.WriteLine("특화 = "+d);
+            Console.WriteLine("제압 = "+e);
+            Console.WriteLine("신속 = "+f);
+            Console.WriteLine("인내 = "+g);
+            Console.WriteLine("숙련 = "+h);
+
+
+
+
+        }
+    }
+}
---
+++ study3/study3/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace study3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //캐릭터
+            //hp : 100
+            //att : 56.7
+            //캐릭터 이름 : ??
+            //등급 : S
+
+            int HP = 100;
+            double pi = 56.7;
+            string name = "박응창";
+            char grade = 'S';
+
+            Console.WriteLine("캐릭터");
+            Console.WriteLine("체력 : "+HP);
+            Console.WriteLine("공격력 : "+pi);
+            Console.WriteLine("캐릭터 이름 : "+name);
+            Console.WriteLine("등급 : "+grade);
+
+
+        }
+    }
+}
