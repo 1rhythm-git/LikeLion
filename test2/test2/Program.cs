f7365a58ea0d6283df60c4bab1c2384c38ee8749
1rhythm-git <dmdckd@gmail.com>
2025-02-24 00:28:22 +0900

    test2

--- test2/test2/Program.cs
+++ test2/test2/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Threading;
 
 namespace test2
 {
@@ -10,6 +11,62 @@ namespace test2
     {
         static void Main(string[] args)
         {
+
+            /*Console.Write("시작하려면 엔터를 누르세요!");
+            Console.ReadLine();
+            Console.Clear();
+
+            Console.WriteLine("○○○○○○○○○○");
+            Thread.Sleep(500);
+            Console.Clear();
+            Console.WriteLine("●○○○○○○○○○");
+            Thread.Sleep(500);
+            Console.Clear();
+            Console.WriteLine("●●○○○○○○○○");
+            Thread.Sleep(500);
+            Console.Clear();
+            Console.WriteLine("●●●○○○○○○○");
+            Thread.Sleep(500);
+            Console.Clear();
+            Console.WriteLine("●●●●○○○○○○");
+            Thread.Sleep(500);
+            Console.Clear();
+            Console.WriteLine("●●●●●○○○○○");
+            Thread.Sleep(500);
+            Console.Clear();
+            Console.WriteLine("●●●●●●○○○○");
+            Thread.Sleep(500);
+            Console.Clear();
+            Console.WriteLine("●●●●●●●○○○");
+            Thread.Sleep(500);
+            Console.Clear();
+            Console.WriteLine("●●●●●●●●○○");
+            Thread.Sleep(500);
+            Console.Clear();
+            Console.WriteLine("●●●●●●●●●○");
+            Thread.Sleep(500);
+            Console.Clear();
+            Console.WriteLine("●●●●●●●●●●");
+            Thread.Sleep(500);
+            Console.Clear();
+            
+
+            Console.WriteLine("성공");*/
+
+
+            int a;
+            float b = 0.0f;
+
+            Console.Write("a를 입력하세요: ");
+            a = int.Parse(Console.ReadLine());
+            Console.Write("b를 입력하세요: ");                     
+            b = float.Parse(Console.ReadLine());
+            Console.Clear();
+
+
+            Console.WriteLine($"a: {a}");
+            Console.WriteLine($"b: {b}%");
+
         }
     }
 }
