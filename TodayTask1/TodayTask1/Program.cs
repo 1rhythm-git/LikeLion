1c84dd9a437dd49577f930e20868f8c11b1a3052
1rhythm-git <dmdckd@gmail.com>
2025-02-23 23:06:31 +0900

    TodayTask1
    사용자 입력을 문자열로 받기

--- TodayTask1/TodayTask1/Program.cs
+++ TodayTask1/TodayTask1/Program.cs
@@ -54,13 +54,17 @@ namespace TodayTask1
             Thread.Sleep(1000);
             Console.Clear();
 
+            Console.WriteLine("이름을 입력하세요: ");
+            string userName = Console.ReadLine();
+            Console.Clear();
+
             Console.WriteLine("고도의 Ai와 VR의 발전");
             Console.ReadLine();
             Console.WriteLine("의학계는 해당 기술을 정신과 치료에 접목하는데 성공하고");
             Console.ReadLine();
             Console.WriteLine("외상 후 스트레스 장애를 치료하는 새로운 VR머신 [드림스퀘어]를 개발한다");
             Console.ReadLine();
-            Console.WriteLine("서로 연인인 연구원 A와 B는 실험을 진행하다가 임상실험의 한계에 부딪히고 결국에 A가 직접 [드림스퀘어]에 입장한다");
+            Console.WriteLine($"서로 연인인 연구원 {userName}와 B는 실험을 진행하다가 임상실험의 한계에 부딪히고 결국에 A가 직접 [드림스퀘어]에 입장한다");
             Console.ReadLine();
             Console.WriteLine("하지만 무슨 이유인지 접속을 종료하지 못하고 자신의 꿈을 기반으로 한 가상의 [드림스퀘어] 세계에서 빠져버린다");
             Console.ReadLine();
@@ -68,9 +72,9 @@ namespace TodayTask1
 
             Console.WriteLine("B: 내말들려?");
             Console.ReadLine();
-            Console.WriteLine("'칠흑 같은 어둠속에 A는 눈을 떴다'");
+            Console.WriteLine($"'칠흑 같은 어둠속에 {userName}는 눈을 떴다'");
             Console.ReadLine();
-            Console.WriteLine("A:.....여기는....??");
+            Console.WriteLine($"{userName}:.....여기는....??");
             Console.ReadLine();
             Console.WriteLine("B: 무슨일인지 넌 지금 드림스퀘어에서 빠져나오지 못하고 있어.");
             Console.ReadLine();
