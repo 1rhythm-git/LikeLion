3f89459a8c4dfd2190d7de627abeab484fe0ec6d
1rhythm-git <dmdckd@gmail.com>
2025-02-24 10:32:42 +0900

    study6

---
+++ study6/study6/Program.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace study6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            /*Console.WriteLine("2진수를 입력하세요: ");
+            string binaryInput = Console.ReadLine();
+            int decimalValue = Convert.ToInt32(binaryInput, 2);
+
+            string binaryOutput = Convert.ToString(decimalValue, 2);
+
+            Console.WriteLine($"입력한 이진수: {binaryInput}");
+            Console.WriteLine($"10진수로 변환: {decimalValue}");
+            Console.WriteLine($"다시 이진수로 변환: {binaryOutput}");*/
+
+
+            /*//var를 사용하여 변수 선언
+            var name = "Alice"; //문자열로 추론
+            var age = 25; //정수로 추론
+            var isStudent = true; //논리 값으로 추론
+
+            Console.WriteLine($"이름: {name}, 나이: {age}, 학생 여부: {isStudent}");*/
+
+
+
+            /* //deafult 키워드를 사용한 기본값 설정
+             int defaultInt = default;   //기본값 : 0
+             string defaultString = default; //기본값 : 0
+             bool defaultBool = default; //기본값 : 0
+
+             defaultInt = int.Parse(Console.ReadLine());
+             defaultString = string.Parse(Console.ReadLine());
+             defaultBool = bool.Parse(Console.ReadLine());
+
+
+             Console.WriteLine($"정수 기본값: {defaultInt}");
+             Console.WriteLine($"정수 기본값: {defaultString}");
+             Console.WriteLine($"정수 기본값: {defaultBool}");*/
+
+
+            /*int a = 5, b = 3;
+            int sum = 0;
+
+            sum = a + b;
+            Console.WriteLine($"합: {sum}");
+
+            sum = a - b;
+            Console.WriteLine($"합: {sum}");
+
+            sum = a * b;
+            Console.WriteLine($"합: {sum}");
+
+            sum = a / b;
+            Console.WriteLine($"합: {sum}");*/
+
+            /* int c = 0, d = 0;
+             int sum = 0;
+
+             c = 10;
+             d = 3;
+             sum = c / d; //나머지를 구하는 연산자
+             Console.WriteLine($"합: {sum}");*/
+
+            /* bool isEqual = (a == b);
+
+             Console.WriteLine($"a와 b가 같은가? {isEqual}");*/
+
+
+            /*int number = 7;
+
+            int sum = 0;
+
+            sum = number % 2; // 0나오면 짝수 1 홀수
+
+            Console.WriteLine($"짝수 홀수 판별 : " + sum);*/
+
+            /*bool isEqual = false;  //거짓 0
+
+            int a = 5;
+            int b = 3;
+
+            //관계형 연산자
+            isEqual = (a == b); //a와 b가 같은가?
+
+            Console.WriteLine("같은가? " + isEqual);*/
+
+
+            
+
+
+
+
+        }
+    }
+}
