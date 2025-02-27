71d8f10031d0727010dd0b58e3c3a6859e24385d
1rhythm-git <dmdckd@gmail.com>
2025-02-27 13:33:50 +0900

    250227_6

---
+++ 250227_6/250227_6/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _250227_6
+{
+    struct Rectangle
+    {
+        public int Width;
+        public int Height;
+
+        public int GetArea() => Width * Height;
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //  var rect = new Rectangle { Width = 5, Height = 4 };
+            Rectangle rect;
+            rect.Width = 10;
+            rect.Height = 20;
+            Console.WriteLine($"Area : {rect.GetArea()}");
+        }
+    }
+}
