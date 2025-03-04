d0aae9ab858b9054f97b2692877468a2c1bb9b87
1rhythm-git <dmdckd@gmail.com>
2025-03-04 17:06:13 +0900

    250304_6_q4

---
+++ 250304__q4/250304__q4/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _250304__q4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string greeting = "Hello";
+            string name1 = "C# World";
+            string name2 = "World";
+
+            string message = greeting + "," + name1 + " " +name2 + "!";
+
+            Console.WriteLine(message);
+
+            Console.WriteLine(greeting + "," + name1.Replace("C#","CSharp")+" "+name2+"!");
+
+
+        }
+    }
+}
