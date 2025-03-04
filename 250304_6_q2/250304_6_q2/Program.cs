52628e9909f062640ebf81a73603237bf5ac068e
1rhythm-git <dmdckd@gmail.com>
2025-03-04 16:33:17 +0900

    250304_6_q2

---
+++ 250304_6_q2/250304_6_q2/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _250304_6_q2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            while(true)
+            {
+                Console.Write("입력: ");
+                string input = Console.ReadLine();
+
+                try
+                {
+                    int num = int.Parse(input); // 값 입력
+                    Console.WriteLine($"출력: {num}"); // 값 출력 
+                }
+                catch(FormatException ex)
+                {
+                    Console.WriteLine($"Format Error: {ex.Message}");
+                }
+                finally
+                {
+                    Console.WriteLine("<완료> \n");
+                }
+            }
+
+        }
+    }
+}
