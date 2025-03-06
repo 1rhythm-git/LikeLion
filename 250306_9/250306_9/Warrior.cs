6f55c24024e8699f2c008c541d2cf03a394e0e7c
1rhythm-git <dmdckd@gmail.com>
2025-03-06 13:05:25 +0900

    250306_9

---
+++ 250306_9/250306_9/Warrior.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _250306_9
+{
+    public class Warrior : GameCharacter
+    {
+        public Warrior(string name) : base(name, 100, 15, 10)
+        {
+        }
+
+        public override void BasicAttack(GameCharacter target)
+        {
+            Console.WriteLine($"{Name}이 {target.Name}에게 마법 구체를 던집니다!");
+            target.TakeDamage(Attack);
+        }
+
+        public override void SpecialAttack(GameCharacter target)
+        {
+            Console.WriteLine($"{Name}이 {target.Name}에 화염 휠윈드 시전합니다.");
+            target.TakeDamage(Attack * 2);
+        }
+    }
+}
