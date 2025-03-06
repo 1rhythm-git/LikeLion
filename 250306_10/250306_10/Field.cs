65739663faa13ba44e940405f1e3fa727c96946d
1rhythm-git <dmdckd@gmail.com>
2025-03-06 15:11:02 +0900

    250306_10
    text rpg

---
+++ 250306_10/250306_10/Field.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _250306_10
+{
+    public class Field
+    {
+        Player m_pPlayer = null;
+        //몬스터
+        Monster m_pMonster = null;
+
+        //플레이어 MainGame에서 생성한 것 가지고 와야함
+        //플레이어가 필드에서 전투하면 데미지 입고 hp가 깎인다
+        //ref를 써보자
+        public void SetPlayer(ref Player pPlayer) { m_pPlayer = pPlayer; }
+
+        public void Progress()
+        {
+            //사냥터로 들어왔다.
+            int iInput = 0;
+
+            while(true)
+            {
+                Console.Clear();
+
+                m_pPlayer.Render();
+                DrawMap();
+
+                iInput = int.Parse(Console.ReadLine());
+
+                if (iInput == 4)
+                    break;
+
+                if(iInput <= 3)
+                {
+                    //몬스터를 만들고
+                    //1번일때 초보몬스터
+                    //2번일때 중급몬스터
+                    //3번일때 고급몬스터
+                    CreateMonster(iInput);
+                    //싸운다
+                    Fight();
+
+                }
+
+            }
+
+            
+        }
+
+
+        //생성을 도와주는 함수
+        //공장처럼 찍어낸다.
+        //팩토리 메서드 패턴
+        public void Create(string _strName, int _iHp, int _iAttack, out Monster pMonster)
+        {
+            pMonster = new Monster();   //몬스터 생성
+            INFO tMonster = new INFO(); //몬스터데이터 메모리 주기 객체 생성
+
+            tMonster.strName = _strName;    //초보몬스터, 중, 고
+            tMonster.iHp = _iHp;    //체력
+            tMonster.iAttack = _iAttack;  //공격력
+
+            pMonster.SetMonster(tMonster);  //생성된 데이터 INFO 클래스타입 이자로 데이터 세팅
+
+        }
+
+
+
+
+
+        public void CreateMonster(int input)
+        {
+            switch(input)
+            {
+                case 1:
+                    //공장처럼 찍어내준다.
+                    //디자인패턴 팩토리 메서드 패턴
+                    Create("초보몹", 30, 3, out m_pMonster);
+                    break;
+                case 2:                    
+                    Create("중급몹", 60, 6, out m_pMonster);
+                    break;
+                case 3:                    
+                    Create("고급몹", 90, 9, out m_pMonster);
+                    break;
+            }
+           
+
+
+
+        }
+
+        public void Fight()
+        {
+            int iInput = 0;
+            while(true)
+            {
+                Console.Clear();    //콘솔화면 지움
+                m_pPlayer.Render(); //플레이어 정보 출력
+                m_pMonster.Render();   //몬스터 정보출력
+
+                Console.WriteLine("1.공격 2.도망 : ");
+                iInput = int.Parse(Console.ReadLine());
+
+                if(iInput == 1)
+                {
+                    //플레이어 데미지 주기 몬스터 공격력 넣어서 데미지
+                    m_pPlayer.SetDamage(m_pMonster.GetMonster().iAttack);
+                    //몬스터 데미지 주기 플레이어 공격력 넣기
+                    m_pMonster.SetDamage(m_pPlayer.GetInfo().iAttack);
+                    if(m_pPlayer.GetInfo().iHp <=0) //플레이어 체력이 0이하면
+                    {
+                        m_pPlayer.SetHp(100);
+                        break;
+                    }
+                }
+
+                if(iInput ==2 || m_pMonster.GetMonster().iHp <=0)
+                {
+                    m_pMonster = null;
+                    break;  //탈출
+                }
+
+            }
+        }
+
+
+
+        public void DrawMap()
+        {
+            Console.WriteLine("1. 초보맵");
+            Console.WriteLine("2. 중수맵");
+            Console.WriteLine("3. 고수맵");
+            Console.WriteLine("4. 전단계");
+            Console.WriteLine("==============");
+            Console.WriteLine("맵을 선택하세요 : ");
+        }
+
+        public Field() { }
+        ~Field() { }
+
+
+
+    }
+}
