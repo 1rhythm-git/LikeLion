9ef71f1dc741320b8f336ee331b27e1838d8958b
1rhythm-git <dmdckd@gmail.com>
2025-03-06 10:18:56 +0900

    250306_5

---
+++ 250306_5/250306_5/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _250306_5
+{
+    
+
+    class Program
+    {
+        //ref 포인터 개념 참조
+        //메서드 ref, out
+        static void Increase(ref int x)
+        {
+            x++;
+        }
+
+        static void Main(string[] args)
+        {
+            int a = 10;
+            Increase(ref a);
+
+            Console.WriteLine("A의 값 : " + a);
+        }
+    }
+}
