2158d32b3638e96d9b9c4b3317b371c79d6f92e9
1rhythm-git <dmdckd@gmail.com>
2025-03-10 08:47:45 +0900

    note1

---
+++ note1/note1/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
+
+namespace _250304_1
+{   
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Hashtable hashtable = new Hashtable();
+
+            //키-값 쌍 추가
+            hashtable["Alice"] = 25;
+            hashtable["Bob"] = 30;
+            hashtable["포션"] = 20;
+
+            Console.WriteLine("Hashtable 요소:");
+            foreach (DictionaryEntry entry in hashtable)
+            {
+                Console.WriteLine($"Key : {entry.Key}, Value : {entry.Value}");
+            }
+
+            //특정 키의 값 가져오기
+            Console.WriteLine($"\n Alice의 나이 : {hashtable["Alice"]}");
+
+            //요소제거
+            hashtable.Remove("Bob");
+
+            Console.WriteLine("Hashtable 요소:");
+            foreach (DictionaryEntry entry in hashtable)
+            {
+                Console.WriteLine($"Key : {entry.Key}, Value : {entry.Value}");
+            }
+        }
+    }
+}
